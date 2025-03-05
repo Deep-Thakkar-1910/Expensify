6a4a5f221331a939d834c1b2595d5887a8d3422c
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2025-03-05 22:43:00 +0530

    fix/change the edit transaction page heading

--- src/app/(Main)/(transactions)/transaction-create/page.tsx
+++ src/app/(Main)/(transactions)/transaction-create/page.tsx
@@ -23,8 +23,8 @@ const CreateTransactionPage = async ({
   }
   return (
     <div className="flex w-full flex-col items-center justify-center px-5 pt-20 lg:pt-28">
-      <h1 className="mb-4 place-self-start bg-gradient-to-r from-fuchsia-700 to-blue-600 bg-clip-text text-4xl font-bold text-transparent lg:text-6xl">
-        Add Transaction
+      <h1 className="gradient-bg mb-4 text-4xl font-bold text-transparent lg:text-6xl">
+        {editMode ? "Edit Transaction" : "Add Transaction"}
       </h1>
       <TransactionForm
         accounts={accounts?.userAccounts || []}
--- src/app/(Main)/dashboard/page.tsx
+++ src/app/(Main)/dashboard/page.tsx
@@ -27,7 +27,7 @@ const DahboardPage = async () => {
   return (
     <>
       <section className="flex size-full h-[100dvh] flex-col items-start justify-start gap-y-4 bg-gradient-to-br from-gray-200 to-white/80 px-8 pt-20 text-black lg:gap-y-8 lg:px-12 lg:pt-28">
-        <h1 className="bg-gradient-to-br from-fuchsia-600 to-blue-600 bg-clip-text text-4xl font-bold text-transparent drop-shadow-md md:text-4xl lg:text-6xl">
+        <h1 className="gradient-bg text-4xl font-bold text-transparent drop-shadow-md md:text-4xl lg:text-6xl">
           Dashboard
         </h1>
         <BudgetProgress
